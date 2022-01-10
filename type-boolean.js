a258b03a8b4e58c67f8031bd7e82fbbdaeb301ab
MEGA\thayz.gardini <thayz.gardini@mega.com.br>
2022-01-10 20:10:20 -0300

    exemplos da aula1

---
+++ type-boolean.js
@@ -0,0 +1,11 @@
+const primeiroNumero = 10;
+const segundoNumero = 10;
+const cadastroAtivado = true;
+
+//console.log(primeiroNumero === segundoNumero);
+
+
+const texto1 = 'Alura';
+const texto2 = 'alura';
+
+console.log(texto1 === texto2);
---
+++ type-string.js
@@ -0,0 +1,17 @@
+const texto1 = "Olá, mundo!";
+const texto2 = 'Olá, mundo!';
+const senha  = 'senha456';
+const citacao = 'O Leo disse "oi"';
+
+// const cidade = "belo horizonte";
+// const input = "Belo Horizonte";
+
+// console.log(cidade === input); // false
+
+
+const cidade = "belo horizonte";
+const input = "Belo Horizonte";
+
+const inputMinusculo = input.toLowerCase();
+
+console.log(cidade === inputMinusculo); // true
